05c34ef681d8b9a5a901c856c8e7fdeaeecbebba
Crystal <crystal_wright@yahoo.com>
2018-01-16 02:28:25 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,38 @@
+function forLoop(array) {
+  for (var i=0; i < 24; i++) {
+    if (i===1) { 
+      array[i] = "I am 1 strange loop."; 
+
+    }
+    else {
+      array[i] = `I am ${i} strange loops.`;
+    }
+  }
+    return array;
+  }
+  
+function whileLoop (num1) {
+  
+  while (num1 >= 0) { 
+    if (num1===0) {return "done";}
+    console.log(num1--)
+  }
+  
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+
+function doWhileLoop (array) {
+  
+  
+  do {
+    array.pop();
+  } while (maybeTrue() && array.length > 0);
+  
+  return array;
+  
+}
+

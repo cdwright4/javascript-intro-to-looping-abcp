9d1dbd37e19746a52a1180daa15a754bfa008500
Crystal <crystal_wright@yahoo.com>
2018-01-16 01:53:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,12 +2,10 @@ function forLoop(array) {
   for (let i=0; i < 25; i++) {
     if (i===1) { 
       array[i] = "I am 1 strange loop."; 
-      console.log(i);
-      
+
     }
     else {
       array[i] = `I am ${i} strange loops.`;
-      //console.log(i);
     }
   }
     return array;
